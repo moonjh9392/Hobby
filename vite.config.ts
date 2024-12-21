0c208476e2f3bfa3bc81cad78967de176c41a661
jhmoon <moonjh@buttercorp.kr>
2024-12-21 17:58:53 +0900

    Setup GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Hobby/", // GitHub 레포지토리 이름으로 수정
   plugins: [react()],
   resolve: {
     alias: {
